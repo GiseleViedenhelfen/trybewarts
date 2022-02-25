0641d52d60beb5abdba6a03c0bf4bfe386d97663
Gisele <gisele.viedenhelfen@gmail.com>
2022-02-25 17:56:15 -0300

    requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const botaoSubmitBody = document.querySelector('#submit-btn');
 const checkboxAgreement = document.querySelector('#agreement');
 const email = document.querySelector('#mail');
 const senha = document.querySelector('#password');
+const textArea = document.querySelector('#textarea');
+const paragrafoContador = document.querySelector('#counter');
 botaoSubmit.addEventListener('click', () => {
   if (senha.value !== '123456' || email.value !== 'tryber@teste.com') {
     alert('Email ou senha inválidos.');
@@ -12,4 +14,10 @@ botaoSubmit.addEventListener('click', () => {
 });
 checkboxAgreement.addEventListener('change', () => {
   botaoSubmitBody.disabled = !checkboxAgreement.checked;
+});
+
+textArea.addEventListener('input', () => {
+  const inputTextArea = textArea.value;
+  const contador = 500;
+  paragrafoContador.innerText = contador - inputTextArea.length;
 });
